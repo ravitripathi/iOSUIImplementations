b798221ddeed77aac036f120b631bf231b46d76d
Ravi Tripathi <ravitripathi1996@gmail.com>
2018-02-20 12:41:17 +0530

    Initial Commit

---
+++ AllUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllUI
+//
+//  Created by Ravi Tripathi on 20/02/18.
+//  Copyright © 2018 Ravi Tripathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
